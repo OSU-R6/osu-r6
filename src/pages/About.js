fe58e06ec8e55df11c8bc450f6a0c998407aae09
Carl Bohme <bohmec@oregonstate.edu>
2023-04-23 22:40:08 -0700

    Ash last name

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ function About() {
             <Row className="alum-info bg-osu-black p-5 mx-0">
                 <Col md={{ span: 12, offset: 0 }}>{
                     <p className="pre-wrap">
-                        The Oregon State Rainbow Six team was first founded in the summer of 2019 when Ash "VonSlakken" LastName founded a collegiate summer team with Carl "Sable" Bohme and fellow OSU students Gavin "SkeDoggle" Gutowsky and Connor "SweatyCarrot" Smith as substitutes. By the fall of 2019, with the addition of Nathan "LawfulNeptune" Berres, OSU had a full roster and had its first offical team in collegiate Rainbow Six.
+                        The Oregon State Rainbow Six team was first founded in the summer of 2019 when Ash "VonSlakken" QuakeLord founded a collegiate summer team with Carl "Sable" Bohme and fellow OSU students Gavin "SkeDoggle" Gutowsky and Connor "SweatyCarrot" Smith as substitutes. By the fall of 2019, with the addition of Nathan "LawfulNeptune" Berres, OSU had a full roster and had its first offical team in collegiate Rainbow Six.
                         <br></br><br></br>
                         In the four years since its founding, Oregon State Rainbow Six has competed in many collegiate leagues including CR6, CEA and the Faceit league sponsored by Ubisoft.
                         <br></br><br></br>
