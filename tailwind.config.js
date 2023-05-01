bdae215e2bd10d5cec9d458dfde6ebdde9130131
Gavin Gutowsky <gtgutowsky@gmail.com>
2023-04-30 19:34:45 -0700

    Created tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          'osu': {
+            'light': '#E05B24',
+            DEFAULT: '#DC4405',
+            'dark': '#C13C04'
+          }
+        },
+      }
+    },
+    plugins: [],
+  }
