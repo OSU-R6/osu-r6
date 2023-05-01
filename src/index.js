3c5dc1d061a71ed33af06faa549a26041c72a66d
Gavin Gutowsky <gtgutowsky@gmail.com>
2023-04-30 19:36:51 -0700

    Added Provider with store to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+)
 
