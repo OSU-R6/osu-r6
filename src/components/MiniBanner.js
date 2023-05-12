9bb514c77ae2bc824158268d3faab8fac77556fa
Carl Bohme <bohmec@oregonstate.edu>
2023-05-12 13:09:43 -0700

    Added bio and stats sections to profile page. Misc styling changes.

--- src/components/MiniBanner.js
+++ src/components/MiniBanner.js
@@ -1,11 +1,11 @@
-function Banner(props) {
+function MiniBanner(props) {
     return (
-        <>
+        <div className="scale-75">
             <div className="py-1 bg-osu-shine" />
             <div className="banner-block">
                 <p className="bg-osu-shine banner-text">{props.children}</p>
             </div>
             <div className="py-1 bg-osu-shine" />
-        </>
+        </div>
     )
-} export default Banner;
+} export default MiniBanner;
--- src/pages/Player.js
+++ src/pages/Player.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import Row from 'react-bootstrap/Row';
 import Carousel from 'react-bootstrap/Carousel';
 import Banner from '../components/Banner'
+import MiniBanner from '../components/MiniBanner'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -30,26 +31,55 @@ function Player() {
 
     return (
         <>
-            <Banner>{player.ign}</Banner>
-            <Carousel slide={false} className="m-4">
-                {clips.map(video => {
-                    return (  
-                        <Carousel.Item interval={null}>
-                        <video autoPlay muted loop className="bg-osu-shine p-1 video-carousel mt-4">
-                            <source src={'http://localhost:8001' + video.link} type="video/mp4" />
-                            Your browser does not support the video tag.
-                        </video>
-                        </Carousel.Item>
-                    )
-                })}   
-            </Carousel>                    
+            <Banner>{player.firstName} "{player.ign}" {player.lastName}</Banner>
+            <div className='flex justify-center items-center m-4'>
+                <div className='w-full'>
+                    <Carousel slide={false} className="">
+                        {clips.map(video => {
+                            return (  
+                                <Carousel.Item interval={null}>
+                                <video autoPlay muted loop className="bg-osu-shine p-1 video-carousel mt-4">
+                                    <source src={'http://localhost:8001' + video.link} type="video/mp4" />
+                                    Your browser does not support the video tag.
+                                </video>
+                                </Carousel.Item>
+                            )
+                        })}   
+                    </Carousel>  
+                </div>
+            </div>
+            <MiniBanner>BIO</MiniBanner>
+            <div className='flex justify-center items-center my-5 grid grid-cols-6 gap-3 bio'>
+                <div className='w-full col-span-6 xl:col-span-2'>
+                    <img className="bannerImage" src="\images\Connor.png"/>
+                </div>
+                <div className='grid-cols-1'/>
+                <div className='w-full col-span-6 xl:col-span-3 preformatted'>
+                    <p className='pre-wrap'>
+                    {player.bio}<br></br>{player.bio}
+                    </p>
+                </div>
+            </div>
+            <MiniBanner>STATS</MiniBanner>
+            <div className='flex justify-center items-center my-5 mx-20 px-20 bio columns-2'>
+                <div className='w-full'>
+                    <img className="bannerImage" src="\images\Connor.png"/>
+                </div>
+                <div className='w-full'>
+                    <img className="bannerImage" src="\images\Connor.png"/>
+                </div>
+            </div>
+            <MiniBanner>CLIPS</MiniBanner>                   
             <Row className='p-4 m-0'>
                 {clips.map(video => {
                     return (  
-                        <video autoPlay muted loop className="bg-osu-shine p-1 mx-auto m-4 video-player">
+                        <>
+                        <div className="clip-title">{video.title}</div>
+                        <video controls muted loop className="bg-osu-shine p-1 mx-auto m-4 video-player">
                             <source src={'http://localhost:8001' + video.link} type="video/mp4" />
                             Your browser does not support the video tag.
                         </video>
+                        </>
                     )
                 })}
             </Row>
