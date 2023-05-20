4e90460936985bbd5a5f2e2f0375611b49dbccf6
Carl Bohme <bohmec@oregonstate.edu>
2023-05-19 22:30:44 -0700

    Home styling.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,11 @@ import Banner from "../components/Banner";
 function Home() {
     return(
         <>
-            <Banner>OREGON STATE RAINBOW SIX</Banner>
+            <Banner>OREGON STATE</Banner>
+            <div>
+                <img className="bannerImage" src="\images\AlumTeam2.png"/>
+            </div>
+            <Banner>RAINBOW SIX</Banner>
         </>
     );
 } export default Home;
