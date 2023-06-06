aac9208ac39e922bc29bd05fba7881b078e7e691
Carl Bohme <bohmec@oregonstate.edu>
2023-06-06 15:59:48 -0700

    Added role to player cards, made player cards components

---
+++ src/components/PlayerCard.js
@@ -0,0 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
+const PlayerCard = ({ player }) => {
+
+    const navigate = useNavigate()
+    console.log(player.ign)
+
+    return (
+        <div className='col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3 2xl:col-span-1 rounded-2xl hover:bg-zinc-950 player-card' onClick={ async () => {navigate('/player/' + player.ign)}}>
+            <div className='flex flex-wrap justify-center'>
+                <div className='player-role text-white r6-font font-semibold w-full text-center text-6xl mt-4'>{player.role}</div>
+                <img className='m-auto player-card-image my-4' src={'http://localhost:8001' + player.pfp} onError={(e) => {e.target.src = './images/placeholderSquish.png'}}/>
+                <div className='player-ign text-white r6-font font-semibold w-full text-center text-7xl mb-4'>{player.ign}</div>
+            </div>
+        </div>
+    )
+} 
+export default PlayerCard
