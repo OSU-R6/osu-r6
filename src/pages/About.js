6bc0074a6dc82992d667b0f1e8e93f0bd2281f49
Carl Bohme <bohmec@oregonstate.edu>
2023-06-22 20:26:35 -0700

    Generate invite functionality

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,15 +7,15 @@ function About() {
             
             <img className="bannerImage" src="\images\AlumTeam.png"/>
             
-            <div className="flex justify-center items-center alum-info bg-osu-black p-5 bio">
+            <div className="flex justify-center items-center text-white bg-osu-black py-5 bio">
                 <div className="w-full">
-                    <p className="pre-wrap r6-font">
+                    <div className="pre-wrap r6-font">
                         The Oregon State Rainbow Six team was first founded in the summer of 2019 when Ash "VonSlakken" QuakeLord founded a collegiate summer team with Carl "Sable" Bohme and fellow OSU students Gavin "SkeDoggle" Gutowsky and Connor "SweatyCarrot" Smith as substitutes. By the fall of 2019, with the addition of Nathan "LawfulNeptune" Berres, OSU had a full roster and had its first offical team in collegiate Rainbow Six.
                         <br></br><br></br>
                         In the four years since its founding, Oregon State Rainbow Six has competed in many collegiate leagues including CR6, CEA and the Faceit league sponsored by Ubisoft.
                         <br></br><br></br>
                         The program has varied in size over the years, growing from a team of five, playing without substitutes, to three full rosters.
-                    </p>
+                    </div>
                 </div>
             </div>
         </>
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,6 @@
 import Banner from '../components/Banner'
+import ErrorMessage from '../components/ErrorMessage'
+
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux'
 import { isloggedIn, getUser } from '../redux/selectors'
@@ -9,15 +11,70 @@ function Admin() {
     const loggedIn = useSelector(isloggedIn)
     const user = useSelector(getUser)
 
+    const API = process.env.REACT_APP_API_URL
+
+    const [ team, setTeam ] = useState("black")
+    const [ invite, setInvite ] = useState("")
+    const [ inviteError, setInviteError ] = useState(false)
+
     useEffect(() => {
     }, [])
 
+    async function inviteHandler() {
+        try{
+            const response = await fetch(API + '/invites/', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ team: team })
+            })
+            if(response.status == 201){
+                const responseBody = await response.json()
+                setInvite(responseBody.success)
+                setInviteError(false)
+            } else {
+                setInviteError(true)
+            }
+        } catch {
+            setInviteError(true)
+        }
+    }
+
+    const copyToClipboard = (invite) => {
+        navigator.clipboard.writeText(invite)
+    }
 
     return(
         <>
             { loggedIn && user.data.admin ? (
             <>
             <Banner>Admin Panel</Banner>
+
+            <div>
+                <form onSubmit={ async (e) => {
+                    e.preventDefault()
+                    await inviteHandler()
+                }} className="flex items-center m-4">
+                    <label className="text-white text-md font-bold px-2" htmlFor="role">Generate User Invite Token</label>
+                    <select className="py-1 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none sm:text-sm mx-2" onChange={e => {setTeam(e.target.value)}} id="team" defaultValue="black" required>
+                        <option value="black">Black Team</option>
+                        <option value="orange">Orange Team</option>
+                        <option value="alumni">Alumni</option>
+                    </select>
+                    <button className="rounded-md bg-osu hover:bg-osu-dark px-10 py-1.5 text-sm font-semibold text-white shadow-sm mx-2" type="submit">Generate</button>
+                    {inviteError && <ErrorMessage>Unable to Generate Invite</ErrorMessage>}
+                </form>
+            </div>
+
+            { invite &&
+                <div className="flex items-center mx-4">
+                    <div className="block py-1 text-white bg-gray-600 rounded px-2 break-all">{invite}</div>
+                    <button className="rounded-md px-10 py-1.5 text-sm font-semibold text-osu shadow-sm" onClick={copyToClipboard(invite)}>Copy</button>
+                </div>
+            }
+
             </>
             ) : ( <Navigate to='/login'/> )}
         </>
