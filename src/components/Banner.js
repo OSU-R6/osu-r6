f2c123731c40526b02e279298c1419f907f4ce77
Carl Bohme <bohmec@oregonstate.edu>
2023-07-11 19:31:53 -0700

    Added navigation to admin panel

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,12 @@
-function Banner(props) {
+const Banner = (props) => {
     return (
         <>
-            <div className="py-1 bg-osu-shine" />
+            <div className={`${props.static ?  "bg-osu" : "bg-osu-shine"} py-1`} />
             <div className="banner-block">
-                <div className="bg-osu-shine banner-text">{props.children}</div>
+                <div className={`${props.static ?  "bg-osu" : "bg-osu-shine"} banner-text text-9xl mb-2`}>{props.children}</div>
             </div>
-            <div className="py-1 bg-osu-shine" />
+            <div className={`${props.static ?  "bg-osu" : "bg-osu-shine"} py-1`} />
         </>
     )
-} export default Banner;
+}
+export default Banner;
---
+++ src/components/EventPanel.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { format } from 'date-fns'
+import DataTable from './DataTable'
+
+const MatchPanel = () => {
+
+    const API = process.env.REACT_APP_API_URL
+
+    const [ events, setEvents ] = useState([])
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
+    const getEvents = async () => { 
+        try{
+            const events = await fetch(API + '/events/upcoming')
+            const eventsBody = await events.json()
+            setEvents(eventsBody)
+        } catch (err) {
+            setEvents([])
+        }
+    }
+
+    const eventColumns=[
+        { field: 'type', headerName: 'Type', flex: 1 },
+        { field: 'description', headerName: 'Description', flex: 1 },
+        { field: 'date', headerName: 'Date', flex: 1, valueFormatter: (params) => format(new Date(params.value), 'MM/dd/yyyy hh:mm a')}
+    ]
+
+    return (
+        <>
+            <DataTable columns={eventColumns} rows={events} />
+        </>
+    )
+} 
+export default MatchPanel
