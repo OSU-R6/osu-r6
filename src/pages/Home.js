2dfb88291f2a7da3fb6e24cfbe0d5193b26c89a4
Carl Bohme <bohmec@oregonstate.edu>
2024-02-29 19:33:01 -0800

    Home cleanup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
-import Banner from "../components/Banner";
 import MatchCard from "../components/MatchCard";
 import EventCard from "../components/EventCard";
-import MiniBanner from '../components/MiniBanner'
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
@@ -44,34 +42,6 @@ function Home() {
 
     return(
         <>
-            {/* <div>
-                <Banner>OREGON STATE</Banner>
-                    <div>
-                        <img className="bannerImage w-full" src="\images\alumteam2.png"/>
-                    </div>
-                <Banner>RAINBOW SIX</Banner>
-            </div> */}
-            {/* <div className="my- text-center">
-                <div className="bg-osu-shine banner-text r6-font text-9xl m-auto pb-2 mb-5">Oregon State Rainbow Six</div>
-                <img className="bannerImage w-full" src="\images\R6_4.png"/>
-            </div> */}
-
-            {/* <div className="mb-5 text-center">
-                <div className="text-white banner-sledge-image r6-font text-9xl m-auto pb-2 mb-5">Oregon State Rainbow Six</div>
-                <Banner><img className="bannerImage w-full" src="\images\R6_4.png"/></Banner>
-            </div> */}
-
-            {/* <div className="m-4 grid grid-cols-12 align-middle items-center">
-                <div className="col-span-12 lg:col-span-4 text-center">
-                    <div className="bg-osu-shine banner-text r6-font text-9xl m-auto pb-2">Oregon State</div>
-                </div>
-                <div className="col-span-12 lg:col-span-4">
-                    <img className="bannerImage w-full scale-75" src="\images\Sledge_Full_Body.webp"/>
-                </div>
-                <div className="col-span-12 lg:col-span-4 text-center">
-                    <div className="bg-osu-shine banner-text r6-font text-9xl m-auto pb-2">Rainbow Six Siege</div>
-                </div>
-            </div> */}
             <div className="home-banner">
                 
                 <div className="m-5 grid grid-cols-12 align-middle items-center">
@@ -84,14 +54,6 @@ function Home() {
                 </div>
                
             </div>
-            
-            {/* <div className="grid grid-cols-12 gap-4 w-75 my-36 mx-auto">
-                <div className="col-span-6 text-center"> 
-                    <div className="text-white r6-font text-6xl"><span onClick={ async () => {navigate('/about/')}}>About The Program</span></div>
-                </div>
-            </div> */}
-  
-            {/* <MiniBanner>Schedule</MiniBanner> */}
 
             <div className='grid grid-cols-12 gap-4 m-4 w-75 mx-auto'>
                 <div className='col-span-12 lg:col-span-6'>
