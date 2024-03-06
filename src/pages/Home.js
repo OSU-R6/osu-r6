09c2228697311dad1744864e1337064ee91c6255
Carl Bohme <bohmec@oregonstate.edu>
2024-03-05 19:57:13 -0800

    Created matches page, redesigned match cards, misc ui fixes.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,8 @@ import MatchCard from "../components/MatchCard";
 import EventCard from "../components/EventCard";
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { Announcement, Stream } from "@mui/icons-material";
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import StreamEmbed from "../components/StreamEmbed";
-import MiniBanner from "../components/MiniBanner";
 import AnnouncementCard from "../components/AnnouncementCard";
 
 function Home() {
@@ -16,6 +15,8 @@ function Home() {
     const [ events, setEvents ] = useState([])
     const [ matchesLive, setMatchesLive ] = useState([])
     const [ announcements, setAnnouncements ] = useState([])
+
+    const scrollElement = document.getElementById('live-match') !== null ? document.getElementById('live-match') : document.getElementById('content');
 
     const navigate = useNavigate()
 
@@ -44,71 +45,75 @@ function Home() {
 
     return(
         <>
-            <div className="home-banner test-banner">  
-                <div className="mx-5 grid grid-cols-12 align-middle items-center h-screen">
-                    <div className="col-span-12 xl:col-span-6 text-center">
-                        <div className="text-white text-shadow-osu osu-glow r6-font md:text-9xl text-7xl m-auto">Oregon State</div>
-                    </div>
-                    <div className="col-span-12 xl:col-span-6 text-center">
-                        <div className="text-white text-shadow-osu osu-glow r6-font md:text-9xl text-7xl m-auto">Rainbow Six</div>
-                    </div>
-                </div>      
-            </div>
+            <div className='relative mb-24'>
+                <div className='home-banner'>  
+                    <div className='mx-5 grid grid-cols-12 align-middle items-center h-screen-80'>
+                        <div className='col-span-12 xl:col-span-6 text-center'>
+                            <div className='text-white text-shadow-osu osu-glow r6-font md:text-9xl text-7xl m-auto'>Oregon State</div>
+                        </div>
+                        <div className='col-span-12 xl:col-span-6 text-center'>
+                            <div className='text-white text-shadow-osu osu-glow r6-font md:text-9xl text-7xl m-auto'>Rainbow Six</div>
+                        </div>
+                    </div>   
+                </div>
+                <div className='absolute left-1/2 bottom-0 transform -translate-x-1/2 '>
+                    <button className='animate-bounce' onClick={() => scrollElement.scrollIntoView({ behavior: 'smooth', block: 'start' })}>
+                        <KeyboardDoubleArrowDownIcon className='text-white text-shadow-osu scale-150'/>
+                    </button>
+                </div>
+            </div>   
 
             { matchesLive.length > 0 &&
-                <div className="live-match">
+                <div className='py-24 relative h-screen sm:h-fit' id='live-match'>
                     <div  className='text-center margin-auto my-4'>
                         <div className='text-white r6-font text-shadow-osu underline md:text-6xl text-5xl'>LIVE MATCH</div>
                     </div>
                     {matchesLive.map((match, i) => {
-                        if (match.stream_link !== null) {
+                        if (match.stream_link !== null && match.stream_link !== '') {
                             return(
                                 <StreamEmbed key={i} match={match} />
                             )
                         }
                     })}
+                    <div className='absolute left-1/2 bottom-10 transform -translate-x-1/2 '>
+                        <button className='animate-bounce' onClick={() => document.getElementById('content').scrollIntoView({ behavior: 'smooth', block: 'start' })}>
+                            <KeyboardDoubleArrowDownIcon className='text-white text-shadow-osu scale-150'/>
+                        </button>
+                    </div>
+                    
                 </div>
             }
 
-            <div className='grid grid-cols-12 gap-4 m-4 mx-auto'>
+            <div className='grid grid-cols-12 gap-4 mx-auto pt-24' id='content'>
                 <div className='col-span-12 xl:col-span-4 grid grid-cols-12 gap-4'>
-                    <div className='col-span-12 lg:col-span-6 xl:col-span-12 mb-4'>
+                    <div className='col-span-12 xl:col-span-12 mb-4'>
                         <div  className='text-center margin-auto'>
-                            <div className='text-white r6-font text-shadow-osu underline md:text-6xl text-5xl'>UPCOMING MATCHES</div>
+                            <div className='text-white r6-font text-shadow-osu underline md:text-6xl text-5xl'>MATCHES</div>
                         </div>
                         <div>
-                            {matchesUpcoming.length > 0 ?
+                            {matchesUpcoming.length > 0 &&
                             <>
                             {matchesUpcoming.slice(0, 3).map((match, i) => {
                                 return (
                                     <MatchCard key={i} match={match} pastMatch={false}/>
                                 )
                             })}
+                            <div className='matches-divider h-1 bg-osu m-auto'></div>
                             </>
-                            :
-                            <div className='text-center text-osu r6-font xl:text-5xl lg:text-4xl text-3xl my-4'>
-                                No Upcoming Matches
-                            </div>
                             }
-                        </div>
-                    </div>
-                    <div className='col-span-12 lg:col-span-6 xl:col-span-12 mb-4'>
-                        <div className='text-center margin-auto'>
-                            <div className='text-white r6-font text-shadow-osu underline md:text-6xl text-5xl'>RECENT MATCHES</div>
-                        </div>
-                        <div>
-                            {matchesPast.length > 0 ?
+                            {matchesPast.length > 0 &&
                             <>
-                            {matchesPast.slice(0, 3).reverse().map((match, i) => {
+                            {matchesPast.slice(0, 3).map((match, i) => {
                                 return (
-                                    <MatchCard key={i} match={match} pastMatch={true}/>
+                                        <MatchCard key={i} match={match} pastMatch={true}/>
                                 )
                             })}
                             </>
-                            :
-                            <div className='text-center text-osu r6-font xl:text-5xl lg:text-4xl text-3xl my-4'>
-                                No Recent Matches
-                            </div>
+                            }
+                            {matchesPast.length > 3 &&
+                                <div className='r6-font text-osu text-3xl text-center'>
+                                    <button onClick={async () => {navigate('/matches')}} className=''>View More</button>
+                                </div>
                             }
                         </div>
                     </div>
@@ -140,14 +145,14 @@ function Home() {
                         <div className='flex'>
                             {events.length > 0 ?
                             <>
-                            {events.map((event, i) => {
+                            {events.slice(0, 2).map((event, i) => {
                                 return (
                                     <EventCard key={i} event={event}/>
                                 )
                             })}
                             </>
                             :
-                            <div className='text-center text-osu r6-font xl:text-5xl lg:text-4xl text-3xl my-4'>
+                            <div className='text-osu r6-font text-4xl m-4'>
                                 No Upcoming Community Events
                             </div>
                             }
